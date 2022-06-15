7d8e77a803ac5c81e50d50a972776ca765ca4eb4
Jeffrey Haugh <jahaugh@mail.fhsu.edu>
2022-06-14 21:02:08 -0500

    Fire bullets from ship.

--- bullet.py
+++ bullet.py
@@ -1,15 +1,15 @@
 import pygame
 from pygame.sprite import Sprite
 
-class Bullet(self):
+class Bullet(Sprite):
     """A class to manage bullets fired from the ship."""
-    
+
     def __init__(self, ai_game):
         """Create a bullet object at the ship's current position."""
         super().__init__()
         self.screen = ai_game.screen
         self.settings = ai_game.settings
-        self.color = self.setting.bullet_color
+        self.color = self.settings.bullet_color
 
         # Create a bullet rect at (0, 0) and then set correct position.
         self.rect = pygame.Rect(0, 0, self.settings.bullet_width, self.settings.bullet_height)
@@ -27,4 +27,4 @@ class Bullet(self):
 
     def draw_bullet(self):
         """Draw the bullet to the screen."""
-        pygame.draw.rect(self.screen, self.color ,self.rect)
+        pygame.draw.rect(self.screen, self.color, self.rect)
